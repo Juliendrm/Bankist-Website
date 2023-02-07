f5feb1b36c7e232c3df4f42ef35e9cfdf2631f01
Julien <benedetjulien@gmail.com>
2023-02-07 19:48:25 +0100

    Smooth scrolling for sections

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -33,7 +35,25 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+//Button Scroll
+
+btnScrollTo.addEventListener("click", function () {
+  section1.scrollIntoView({ behavior: "smooth" });
+});
 const header = document.querySelector(".header");
+
+//Page navigation
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    //console.log("link");
+    //console.log(e.target);
+    const id = e.target.getAttribute("href");
+    //console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
 
 /*
 console.log(document.querySelector(".header"));
@@ -62,32 +82,24 @@ message.style.width = "110%";
 document.documentElement.style.setProperty("--color-primary", "orangered");
 
 //Attributes
-const logo = document.querySelector(".nav__logo");
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
+//const logo = document.querySelector(".nav__logo");
+//console.log(logo.alt);
+//console.log(logo.src);
+//console.log(logo.className);
 
 logo.alt = "Beautiful logo";
 
 //Non-standard
-console.log(logo.designer);
-console.log(logo.getAttribute("designer"));
-console.log(logo.getAttribute("src"));
+//console.log(logo.designer);
+//console.log(logo.getAttribute("designer"));
+//console.log(logo.getAttribute("src"));
 logo.setAttribute("company", "Bankist");
 const link = document.querySelector(".nav__link--btn");
-console.log(link.href);
-console.log(link.getAttribute("href"));
+//console.log(link.href);
+//console.log(link.getAttribute("href"));
 
 //Data Attributes
-console.log(logo.dataset.versionNumber);
-
-//Smooth Scrolling
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
-btnScrollTo.addEventListener("click", function () {
-  section1.scrollIntoView({ behavior: "smooth" });
-});
+//console.log(logo.dataset.versionNumber);
 
 //Handling Events
 
@@ -99,4 +111,29 @@ const alertH1 = function (e) {
 
 h1.addEventListener("mouseenter", alertH1);
 
-setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 5000);
+setTimeout(() => h1.removeEventListener("mouseenter", alertH1));
+
+//Random Color
+/*
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+  console.log("link", e.target, e.currentTarget);
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  console.log("container", e.target, e.currentTarget);
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector(".nav").addEventListener("click", function (e) {
+  console.log("nav", e.target, e.currentTarget);
+  this.style.backgroundColor = randomColor();
+});
+*/
