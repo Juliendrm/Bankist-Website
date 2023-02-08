19c75c59593e14187604e59ed6f84d082f985ff3
Julien <benedetjulien@gmail.com>
2023-02-08 15:49:33 +0100

    sticky bar scroll with coordinates of section 1, but can lead to bad performance

--- script.js
+++ script.js
@@ -97,11 +97,22 @@ const handleHover = function (e) {
     logo.style.opacity = this;
   }
 };
-
 nav.addEventListener("mouseover", handleHover.bind(0.5));
-
 nav.addEventListener("mouseout", handleHover.bind(1));
 
+///Sitcky nav
+
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+});
 /*
 console.log(document.querySelector(".header"));
 const allSections = document.querySelectorAll(".section");
